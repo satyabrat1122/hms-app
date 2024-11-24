f2f4f3e7e279efddc9f262f0690153d08627eb60
Satya <satyabrat1202@gmail.com>
2024-11-24 09:56:40 +0530

    Java Class -24-nov-2024

--- src/main/java/com/hms/services/WhatsAppService.java
+++ src/main/java/com/hms/services/WhatsAppService.java
@@ -1,6 +1,6 @@
 package com.hms.services;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
